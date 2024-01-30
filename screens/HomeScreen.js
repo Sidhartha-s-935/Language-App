2dafceb1a95a7570e25d4e00e7bc8871800a0959
Sidhartha-S <sidharthas935@gmail.com>
2024-01-30 15:59:22 +0530

    chat

---
+++ screens/HomeScreen.js
@@ -0,0 +1,104 @@
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React, { useLayoutEffect, useContext, useEffect, useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { Ionicons } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
+import { UserType } from "../UserContext";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import jwt_decode from "jwt-decode";
+import axios from "axios";
+import User from "../components/User";
+const HomeScreen = () => {
+  const navigation = useNavigation();
+  const { userId, setUserId } = useContext(UserType);
+  const [users, setUsers] = useState([]);
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerTitle: "",
+      headerLeft: () => (
+        <Text style={{ fontSize: 16, fontWeight: "bold" }}>
+          LanguageLearner
+        </Text>
+      ),
+      headerRight: () => (
+        <View style={{ flexDirection: "row", alignItems: "center", gap: 8 }}>
+          <Ionicons
+            onPress={() => navigation.navigate("Chats")}
+            name="chatbox-ellipses-outline"
+            size={24}
+            color="black"
+          />
+          <MaterialIcons
+            onPress={() => navigation.navigate("Friends")}
+            name="people-outline"
+            size={24}
+            color="black"
+          />
+        </View>
+      ),
+    });
+  }, []);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const token = await AsyncStorage.getItem("authToken");
+      const decodedToken = jwt_decode(token);
+      const userId = decodedToken.userId;
+      setUserId(userId);
+
+      axios
+        .get(`http://10.0.2.2:8000/users/${userId}`)
+        .then((response) => {
+          setUsers(response.data);
+        })
+        .catch((error) => {
+          console.log("error retrieving users", error);
+        });
+    };
+
+    fetchUsers();
+  }, []);
+
+  console.log("users", users);
+
+  const handleLogout = async () => {
+    try {
+      // Send a request to the backend logout endpoint
+      await axios.post("http://10.0.2.2:8000/logout");
+
+      // Clear the stored authentication token
+      await AsyncStorage.removeItem("authToken");
+
+      // Navigate the user to the login screen
+      navigation.replace("Login"); // Replace with the name of your login screen
+    } catch (error) {
+      console.log("Error during logout", error);
+    }
+  };
+
+  return (
+    <View>
+      <View style={{ padding: 10 }}>
+        {users.map((item, index) => (
+          <User key={index} item={item} />
+        ))}
+      </View>
+      {/* Logout Button */}
+      <TouchableOpacity
+        onPress={handleLogout}
+        style={{
+          padding: 10,
+          backgroundColor: "red",
+          alignItems: "center",
+          marginTop: 20,
+        }}
+      >
+        <Text style={{ color: "white" }}>Logout</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({});
