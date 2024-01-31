b10bd906be75c87009da2b5c6216de2e8fc7991d
Sidhartha-S <sidharthas935@gmail.com>
2024-01-31 16:21:50 +0530

    LearnPage

--- screens/LearnPage.js
+++ screens/LearnPage.js
@@ -1,12 +1,45 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable } from "react-native";
 import React from "react";
+import { useNavigation } from "@react-navigation/native";
 
 const LearnPage = () => {
+  const navigation = useNavigation();
   return (
-    <View>
-      <Text>LearnPage</Text>
+    <View
+      style={{
+        flex: 1,
+        flexDirection: "row",
+        alignItems: "flex-start",
+        justifyContent: "center",
+        marginTop: 15,
+      }}
+    >
+      <Pressable
+        onPress={() => navigation.navigate("Hiragana")}
+        style={buttonStyle}
+      >
+        <Text style={buttonTextStyle}>Hiragana</Text>
+      </Pressable>
+      <Pressable
+        onPress={() => navigation.navigate("Katakana")}
+        style={buttonStyle}
+      >
+        <Text style={buttonTextStyle}>Katakana</Text>
+      </Pressable>
     </View>
   );
+};
+
+const buttonStyle = {
+  backgroundColor: "#3498db",
+  padding: 50,
+  margin: 12,
+  borderRadius: 25,
+};
+
+const buttonTextStyle = {
+  color: "#ffffff",
+  fontSize: 20,
 };
 
 export default LearnPage;
